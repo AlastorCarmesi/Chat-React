a0ee1fddd13712fdbb2b2ad28558f5cde5e1e891
AlastorCarmesi <carmesiblog7@gmail.com>
2023-07-06 11:55:14 -0600

    Modificaciones

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 //Modulos necesarios para el chat
 const express = require("express");
 const http = require('http');
-const {Server} = require("socket.io");
 import { Server as SocketServer } from "socket.io";
 
 const app = express();
 const io = new Server(server);
 const server = http.createServer(app);
+
+io.on('connection', socket => {
+    console.log('usuario conectado...')
+})
 
 server.listen(4000)
 console.log('Server on Port', 4000)
